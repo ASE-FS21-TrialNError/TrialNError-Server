a444a569f65870141b62d09cdb080c9d004c9e39
tanzilkm <tanzilkmkm@gmail.com>
2021-04-19 14:59:35 +0200

    Add name as filter

--- src/apps/apps.controller.ts
+++ src/apps/apps.controller.ts
@@ -1,10 +1,7 @@
 import { Controller, Get, UseGuards,Query,ParseIntPipe, Param } from '@nestjs/common';
 import { AppsService } from './apps.service';
-import { Apps } from './models/apps';
-import { AuthUser } from 'src/common/decorators/auth-user.decorator';
 import { JwtAuthGaurd } from 'src/common/utils/helpers';
 import { ApiBearerAuth } from '@nestjs/swagger';
-import { ResponseSuccess } from 'src/common/dto/response.dto';
 
 
 @ApiBearerAuth()
@@ -18,6 +15,7 @@ export class AppsController {
     @Query('page', new ParseIntPipe()) page?: number,
     @Query('limit', new ParseIntPipe()) limit?: number,
     @Query('sort') sort?: string,
+    @Query('name') name?: string,
     @Query('category_andr') category_andr?: string,
     @Query('category_ios') category_ios?: string,
     @Query('price_ios') price_ios?: string,
@@ -30,7 +28,7 @@ export class AppsController {
     @Query('content_rating_andr') content_rating_andr?: string,
     
   ) {
-    return this.appsService.getAllApps(page, limit,sort,category_andr,category_ios,price_andr,price_ios,rating_andr,rating_ios,rating_count_andr,rating_count_ios,content_rating_andr,content_rating_ios);
+    return this.appsService.getAllApps(page, limit,sort,name,category_andr,category_ios,price_andr,price_ios,rating_andr,rating_ios,rating_count_andr,rating_count_ios,content_rating_andr,content_rating_ios);
   }
 
   @Get(':id')
