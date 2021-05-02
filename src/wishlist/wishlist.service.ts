6f67ea5d0b2af211cad7ffe7bdac5465dbf7c29f
tanzilkm <tanzilkmkm@gmail.com>
2021-05-02 16:01:12 +0200

    check App ID is correct

--- src/wishlist/wishlist.service.ts
+++ src/wishlist/wishlist.service.ts
@@ -3,13 +3,17 @@ import { Wishlist } from './models/wishlist';
 import { InjectModel } from 'nestjs-typegoose';
 import { ReturnModelType } from '@typegoose/typegoose';
 import { UserDto } from '../users/dto/user.dto';
-import { ResponseSuccess } from 'src/common/dto/response.dto';
+import { ResponseSuccess } from '../common/dto/response.dto';
+import { Apps} from '../apps/models/apps';
+
 
 @Injectable()
 export class WishlistService {
   constructor(
     @InjectModel(Wishlist) 
     private readonly wishlistModel: ReturnModelType<typeof Wishlist>,
+    @InjectModel(Apps) 
+    private readonly appsModel: ReturnModelType<typeof Apps>,
   ) {}
   async addApp(appId: string,user: UserDto) {
       const wishList = await this.wishlistModel.findOne({userEmail:user.email})
@@ -17,6 +21,10 @@ export class WishlistService {
               throw new HttpException('WishList Not Found', HttpStatus.NOT_FOUND);
             })
             .then(result => result);
+      await this.appsModel.findById(appId)
+      .catch(err => {
+        throw new HttpException('App not found', HttpStatus.NOT_FOUND);
+      })
       let apps=wishList.apps;
       apps.push(appId);
       apps=apps.filter((v, i, a) => a.indexOf(v) === i);
@@ -30,7 +38,10 @@ export class WishlistService {
             throw new HttpException('WishList Not Found', HttpStatus.NOT_FOUND);
           })
           .then(result => result);
-
+          await this.appsModel.findById(appId)
+          .catch(err => {
+            throw new HttpException('App not found', HttpStatus.NOT_FOUND);
+          })
     let apps=wishList.apps;
     const index = apps.indexOf(appId);
       if (index > -1) {
