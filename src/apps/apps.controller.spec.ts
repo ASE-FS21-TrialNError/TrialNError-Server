38c43b84160b0d9db4f0a9e34704aa985b23a9d7
Vladimir Donkov <vladimir.donkov1992@gmail.com>
2021-05-21 16:57:18 +0200

    Testing

--- src/apps/apps.controller.spec.ts
+++ src/apps/apps.controller.spec.ts
@@ -3,24 +3,46 @@ import { AppsController } from './apps.controller';
 import { AppsService } from './apps.service';
 
   describe('AppsController', () => {
-    let appsService: AppsService;
+    // let appsService: AppsService;
     let appsController: AppsController;
   
+    const mockAppsService = {
+      getAllApps: jest.fn((page, limit) => {
+        return {
+          totalPages: 500,
+          items: [],
+          total: 4999,
+        }
+      })
+    };
+
     beforeEach(async () => {
       const module: TestingModule = await Test.createTestingModule({
        controllers: [AppsController],
-        providers:  [
-          {
-             provide: AppsService,
-             useValue: {},
-          },
-         ],
-      }).compile();
-      appsService = module.get<AppsService>(AppsService);
+        providers:  [AppsService],
+      })
+      .overrideProvider(AppsService)
+      .useValue(mockAppsService)
+      .compile();
+      // appsService = module.get<AppsService>(AppsService);
       appsController = module.get<AppsController>(AppsController);
       
     });
+
+    it('AppsController should be defined', () => {
+      expect(appsController).toBeDefined();
+    });
   
+    it('dont know yet', () => {
+      expect(appsController.getAllApps(1,10)).toEqual({
+        totalPages: 500,
+        items: [],
+        total: 4999,
+      });
+
+      expect(mockAppsService.getAllApps).toHaveBeenCalled();
+    });
+
     describe('Apps', () => {
       it('List  Apps',async  () => {
         let result ;
